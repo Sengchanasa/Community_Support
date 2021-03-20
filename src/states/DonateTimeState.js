8c34451356ff3f1d52cc8068c5fc7daeb76998b6
Trang <quynhtranghbt@gmail.com>
2021-03-20 02:49:33 -0400

    donate time kinda works

--- src/states/DonateTimeState.js
+++ src/states/DonateTimeState.js
@@ -1,5 +1,60 @@
-module.exports = {
+const { getAllOrganizationEvents } = require("../api");
 
+module.exports = {
+  DonateTimeIntent(){
+    let communityName;
+    let input = this.$inputs.communityName.value;
+    
+    if (input != null){
+      communityName = input;
+    }else if(this.$session.$data.communityName != null) {
+      communityName = this.$session.$data.communityName;
+      console.log( "check inside else if"+JSON.stringify( this.$session.$data))
+
+
+    }
+    //Get commuinities if there no session data or input
+    else{
+       // create 2 session variables
+      this.$session.$data.previousState = this.getState();
+      this.$session.$data.previousIntent = "DonateTimeIntent";
+      //Prompt for community name
+
+      return this.ask("What is the name of the community you want to support, black or asian?")
+
+    }
+
+    
+    this.$speech.addText(`You can donate your time to the ${communityName} community by `);
+    let events = getAllOrganizationEvents(communityName);
+    let eventsList="";
+    console.log("check " + events)
+    for (i in events){
+
+      eventsList += events[i].name + ", "
+    }
+    this.$speech.addText("Joining these upcoming events " + eventsList);
+
+
+    this.followUpState("DonateTimeState.Confirmation");
+
+    this.$speech.addText("Would you like me to send you the email of these events?")
+    
+    return this.ask(this.$speech)
+
+
+  }, 
+  Confirmation:{
+
+    YesIntent(){
+      this.toStateIntent("WelcomeState", "WelcomeIntent")
+   
+    },
+
+    NoIntent(){
+      this.ask("No problem!")
+    }
+  }
 
 
 }
