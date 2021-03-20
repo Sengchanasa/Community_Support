e94f04a56ef0e697ff2d4ec04fe52d0c8af7df49
Sengchanasa <joshualuangasa@gmail.com>
2021-03-20 00:21:19 -0400

    Updated api and json

--- src/api.js
+++ src/api.js
@@ -1,12 +1,43 @@
 const resources = require("./data/resources.json")
+console.log("hello world")
 
-module.exports = {
-    getAllOrganizations() {
-        let ret = "";
-        for(i in resources.organizations) {
-            ret = ret + resources.organizations[i].name + " Movement, or ";
+console.log(getOrganization("blackCommunity"))
+    module.exports = {getOrganization}
+
+    //Gets organization name in database app currently supports
+    function getOrganization(community, organization) {
+        for(i in resources[community].organizations) {
+            if (resources[community].organizations[i].name==organization) {
+                return resources[community].organizations[i];
+            }
         }
-        return ret;
     }
-}
+    
+    //Gets all organizations in database app currently supports
+    function getAllOrganizations(community) {
+        //console.log("inside get all org names")
+        let ret = "";
+        for(i in resources[community].organizations) {
+            ret = ret + resources[community].organizations[i].name + " ";
+        }
+        return "[ " + ret + "]";
+    }
+
+    //Gets the description for an organization.
+    function getOrganizationDescription(community, organization) {
+        let org = getOrganization(community, organization)
+
+        //console.log(JSON.stringify(name))
+        return  org.name + ": " + org.description;
+    }
+
+    //Gets all handles that organization[organization] is in
+    function getOrganizationHandles(community, organization) {
+        let org = getOrganization(community, organization)
+        let ret = ""
+        for(i in org.handle) {
+            ret = ret + org.handle[i] + " ";
+        }
+        return "[ " + ret + "]";
+    }
 
